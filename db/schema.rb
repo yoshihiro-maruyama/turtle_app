7e77aabe806c85eaa8184032dae3e94b1ec424f9
Yoshihiro.Maruyama <y.maruyama0008@gmail.com>
2020-09-10 14:21:43 +0900

    Add secure passwords

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_072855) do
+ActiveRecord::Schema.define(version: 2020_09_10_040904) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
